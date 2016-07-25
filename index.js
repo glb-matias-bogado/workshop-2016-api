d8ea696eab1d6671bdbf950365033462cfc5d9cc
Matias Bogado <Matias.Bogado@wnco.com>
2016-07-25 13:28:57 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+const PORT = 1338;
+
+const cors = require('cors');
+//const Datastore = require('nedb');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const CommentsService = require('./core/services/comments');
+const api = require('./api');
+
+/*const db = new Datastore({
+  filename: 'datastore/workshop.db',
+  autoload: true
+});*/
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/bands', api.bandsRoutes(db, commentsSrv));
+app.use('/albums', api.albumsRoutes(db, commentsSrv));
+app.use('/tracks', api.tracksRoutes(db, commentsSrv));
+app.use('/artists', api.artistsRoutes(db, commentsSrv));
+app.use('/comments', api.commentsRoutes(commentsSrv));
+
+app.listen(PORT, () => console.log(`App started and listening on port ${PORT}`));
